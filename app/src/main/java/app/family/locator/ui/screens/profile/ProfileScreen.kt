e66e791434a2e8f80ec06cccfce2572202ff3f2b
Prasanna Anbazhagan <panbazhagan@twilio.com>
2022-09-15 09:11:43 +0530

    save commit

--- app/src/main/java/app/family/locator/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/app/family/locator/ui/screens/profile/ProfileScreen.kt
@@ -1,8 +1,9 @@
 package app.family.locator.ui.screens.profile
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun ProfileScreen() {
-
+    Text(text = "TODO. Under construction")
 }
